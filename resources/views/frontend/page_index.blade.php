4b9c31e76bf8312b331688a6d387c9921451598b
Crhistian Vargas <cristian-d-2@hotmail.com>
2023-07-06 19:54:21 -0500

    CAMBIO DE ICONOS Y MEJORA DE RESPONSIVE DEL HOME

--- resources/views/frontend/page_index.blade.php
+++ resources/views/frontend/page_index.blade.php
@@ -12,16 +12,15 @@
                         <div class="single-slide d-flex align-items-center bg-image"
                             data-bg-image="assets/img/slider/back_full2023.png">
                             <div class="container">
-
-                                <div class="row align-items-center no-gutters w-100">
-                                    <div class="col-lg-6  offset-lg-3 col-md-5">
+                                <div class="row align-items-center no-gutters w-100 mt-5">
+                                    <div class="col-lg-12 col-md-12" style="margin-left: 28% !important">
                                         <figure class="slider-image d-none d-md-block">
-                                            <img src="{{ asset('assets/img/slider/logo-2023-03.png')}}" alt="Slider Image">
+                                            <img src="{{ asset('assets/img/slider/logo-2023-03.png')}}" alt="Slider Image" style="">
                                         </figure>
                                     </div>
                                 </div>
                                 <div class="row align-items-center no-gutters w-100  mt-5">
-                                    <div class="col-lg-6 offset-lg-5 col-md-5">
+                                    <div class="col-lg-12 offset-lg-5 col-md-12">
                                         <div class="slider-content__btn">
                                             <a href="{{ URL::to('loginview') }}" class="css-button" style="background: #232E84 !important;" data-animation="fadeInUp">EMPEZÁ</a>
                                         </div>
@@ -87,12 +86,12 @@
 
                         <div class="col-md-4 mb-sm--50">
                             <div class="text-align-center" style="max-width:50%;width:auto;height:auto;">
-                                <a href="{{ URL::to('aprende') }}" >
+                                {{--  <a href="{{ URL::to('aprende') }}" >
                                     <img src="{{ asset('assets/img/brand/ico 5.png')}}" alt="Eventos">
                                     <div class="product__overlay"></div>
                                     <div class="btn-link" data-animation="fadeInUp"
                                     data-duration=".3s" data-delay=".6s">APRENDE</div>
-                                </a>
+                                </a>  --}}
                             </div>
                         </div>
 
