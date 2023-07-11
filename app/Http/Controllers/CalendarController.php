80fc6edf4c3758961d66c67785b7d073d86dc1af
gzuluaga <thekurai@gmail.com>
2023-07-11 17:18:03 -0500

    primer canbio

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -22,7 +22,6 @@ class CalendarController extends Controller
     public function calendarEvents(Request $request)
     {
        $eventos = Calendars::get();
-
        return ['eventos' => $eventos];
     }
 
