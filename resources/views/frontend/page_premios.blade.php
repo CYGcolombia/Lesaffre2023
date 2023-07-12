b658ad24c39a6575c96e68588a914c62350cc366
Crhistian Vargas <cristian-d-2@hotmail.com>
2023-07-11 20:15:08 -0500

    ACTUALIZACION DE LAS IMAGENES PARA LOS PREMIOS Y AJUSTE DE LA PAGINA

--- resources/views/frontend/page_premios.blade.php
+++ resources/views/frontend/page_premios.blade.php
@@ -4,23 +4,18 @@
         
 <!-- Main Wrapper Start -->
 <div class="wrapper">
-    <div class="single-slide d-flex align-items-center bg-image" data-bg-image="assets/img/premios/beach-product-background.jpg" style=" background-position: top !important; margin-top: 0% !important;">                    
+    <div class="single-slide d-flex align-items-center bg-image" data-bg-image="assets/img/premios/beach-product-background.png" style=" background-position: top !important; margin-top: 0% !important;">                    
         <!-- Main Content Wrapper Start -->
-        <main class="main-content-wrapper espacio-menu">
-            <div class="row" style="margin-top: 10%">
-                {{--  <div class="paralelogramo" style="margin-bottom: 0% !important;">Premios</div>  --}}
-            </div>
-
-            <div class="row justify-content-center"></div>
+        <main class="main-content-wrapper">
+\
                 <center>
-                    <img src="{{ asset('assets/img/premios/premios_new.png') }}" alt="Premios" style="width: 65%; max-width: 100%; max-height: 100%;" class="responsive-footer">
+                    <img src="{{ asset('assets/img/premios/premios_new.png') }}" alt="Premios" style="width: 100%; max-width: 100%; max-height: 100%;" class="responsive-footer">
                 </center>
-
-                    <div class="row justify-content-center">
+                    <div class="row justify-content-center" style="background-color: white !important; z-index: 1000 !important; margin-top: -11vh;">
                         <div class="col-lg-9">
                             <article class="mb-md--55 pb-md--55">                                        
                                 <div class="single-post__info">
-                                    <div class="row mb--20 mb-md-33 mt--20">
+                                    <div class="row mb--20 mb-md-33 mt-5">
                                         <div class="col-lg-12 col-md-12">
                                             <p align="justify" style="font-size: 15px !important;">
                                                 El Desafío Lesaffre 2023 premiará al distribuidor que alcance la mejor posición en el concurso con un increíble viaje.
